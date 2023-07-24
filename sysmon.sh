ebae111031470b004223226ceab9ca791ba74bfa
zane8n <65125782+zane8n@users.noreply.github.com>
2023-07-24 14:42:16 +0300

    Update sysmon.sh | dependancies

--- sysmon.sh
+++ sysmon.sh
@@ -62,6 +62,8 @@ if [ "${ARCHITECTURE}" = 'x86_64' ]; then
       fi
       case "$DIST_VERSION" in
         18.04 | 20.04 | 21.04)
+        #The following line has been added to install dependancies
+          eval $SUDO $pkgMgr build-essential gcc g++ make cmake libelf-dev llvm clang libxml2 libxml2-dev libzstd1 git libgtest-dev apt-transport-https dirmngr monodevelop googletest google-mock libgmock-dev libjson-glib-dev
           wget -q https://packages.microsoft.com/config/ubuntu/$DIST_VERSION/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
           eval $SUDO dpkg -i packages-microsoft-prod.deb
           eval $SUDO apt-get update
